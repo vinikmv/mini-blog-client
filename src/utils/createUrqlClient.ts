5cb82e091af727ae2fd9658bd1420e7d4aab5e69
viniciuskmv <viniciuskmv@gmail.com>
2020-12-03 11:36:13 -0300

    feat: invalidate cache of post query on index page when creating a new post

--- src/utils/createUrqlClient.ts
+++ src/utils/createUrqlClient.ts
@@ -85,6 +85,15 @@ export const createUrqlClient = (ssrExchange: any) => ({
       },
       updates: {
         Mutation: {
+          createPost: (_result, _args, cache, _info) => {
+            const allFields = cache.inspectFields("Query");
+            const fieldInfos = allFields.filter(
+              info => info.fieldName === "posts"
+            );
+            fieldInfos.forEach(field => {
+              cache.invalidate("Query", "posts", field.arguments || {});
+            });
+          },
           logout: (_result, _args, cache, _info) => {
             betterUpdateQuery<LogoutMutation, MeQuery>(
               cache,
